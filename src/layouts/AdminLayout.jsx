f96700a95ba465ea91d2b11446af8600cb0a4b7d
Rahmat Hussain <rahmathussain.hjp@gmail.com>
2026-03-05 20:26:44 -0500

    Admin layout updated

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -3,19 +3,6 @@ import { Outlet } from "react-router-dom";
 function AdminLayout() {
   return (
     <div style={{ display: "flex", minHeight: "100vh" }}>
-      
-      {/* Sidebar */}
-      <div style={{ width: "250px", background: "#111827", color: "white", padding: "20px" }}>
-        <h2>ServDial Admin</h2>
-
-        <ul style={{ listStyle: "none", padding: 0 }}>
-          <li><a href="/admin/dashboard">Dashboard</a></li>
-          <li><a href="/admin/add-business">Add Business</a></li>
-          <li><a href="/admin/add-city">Add City</a></li>
-          <li><a href="/admin/add-category">Add Category</a></li>
-          <li><a href="/admin/banners">Banners</a></li>
-        </ul>
-      </div>
 
       {/* Main Area */}
       <div style={{ flex: 1 }}>
