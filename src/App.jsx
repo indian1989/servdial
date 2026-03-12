48a45f7ccc8c81c1da19644f5660933155538870
Rahmat Hussain <rahmathussain.hjp@gmail.com>
2026-03-11 20:33:20 -0400

    add the fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ import { BusinessProvider } from "./context/BusinessContext";
 import { CityProvider } from "./context/CityContext";
 import { CategoryProvider } from "./context/CategoryContext";
 import AppRoutes from "./routes/AppRoutes";
-import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <BusinessProvider>
-        <CityProvider>
-          <CategoryProvider>
-            <AppRoutes />
-          </CategoryProvider>
-        </CityProvider>
-      </BusinessProvider>
-    </Router>
+    <BusinessProvider>
+      <CityProvider>
+        <CategoryProvider>
+          <AppRoutes />
+        </CategoryProvider>
+      </CityProvider>
+    </BusinessProvider>
   );
 }
 
