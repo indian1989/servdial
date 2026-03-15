33bd0c68cb9c30497491faf70defa17ef1183ee5
Rahmat Hussain <rahmathussain.hjp@gmail.com>
2026-03-15 12:48:48 -0400

    add this

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL ||
-    "https://servdial-backend.onrender.com/api",
+    "https://https://servdial-backend.onrender.com//api",
   timeout: 60000,
   headers: {
     "Content-Type": "application/json",
